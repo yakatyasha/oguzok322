22e58767964008726db95506cd692a7e1a50356d
yakatyasha <127683941+yakatyasha@users.noreply.github.com>
2023-05-20 08:47:40 +0300

    Add files via upload

---
+++ MathTest/WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        int addend1, addend2;
+        int minuend, subtrahend;
+        int multiplicand, multiplier;
+        int dividend, divisor;
+        int timeLeft;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timeLabel_Click(object sender, EventArgs e)
+        {
+            if(CheckTheAnswer())
+            {
+                timer1.Stop();
+                MessageBox.Show("Вы ответили на все вопросы", "ОЙ!");
+                startButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                timeLeft = timeLeft - 1;
+                timeLabel.Text = timeLeft +"seconds";
+            }
+            else
+            {
+                timer1.Stop();
+                timeLabel.Text = "Время закончилось!";
+                MessageBox.Show("Вы не успели", ":С");
+                sum.Value = addend1 + addend2;
+                difference.Value = minuend - subtrahend;
+                product.Value = multiplicand * multiplier;
+                quotient.Value = dividend / divisor;
+                startButton.Enabled = true;
+            }
+        }
+
+        private void answer_Enter(object sender, EventArgs e)
+        {
+            NumericUpDown answerBox = sender as NumericUpDown;
+
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startButton_Click_1(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
+        }
+
+        
+
+        public void StartTheQuiz()
+        {
+           
+            addend1 = r.Next(51);
+            addend2 = r.Next(51);
+
+         
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+
+           
+            sum.Value = 0;
+
+            
+            minuend = r.Next(1, 101);
+            subtrahend = r.Next(1, minuend);
+            minusLeftLabel.Text = minuend.ToString();
+            minusRightLabel.Text = subtrahend.ToString();
+            difference.Value = 0;
+
+            
+            multiplicand = r.Next(2, 11);
+            multiplier = r.Next(2, 11);
+            timesLeftLabel.Text = multiplicand.ToString();
+            timesRightLabel.Text = multiplier.ToString();
+            product.Value = 0;
+
+          
+            divisor = r.Next(2, 11);
+            int temporaryQuotient = r.Next(2, 11);
+            dividend = divisor * temporaryQuotient;
+            dividedLeftLabel.Text = dividend.ToString();
+            dividedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
+
+           
+            timeLeft = 30;
+            timeLabel.Text = "30 seconds";
+            timer1.Start();
+        }
+        private bool CheckTheAnswer()
+        {
+            if ((addend1 + addend2 == sum.Value)
+                && (minuend - subtrahend == difference.Value)
+                && (multiplicand * multiplier == product.Value)
+                && (dividend / divisor == quotient.Value))
+                return true;
+            else
+                return false;
+        }
+
+       
+    }
+}
