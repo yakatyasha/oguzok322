31f4a67198853ebd2430588ef18d65294a7b76a2
yakatyasha <127683941+yakatyasha@users.noreply.github.com>
2023-05-20 08:44:32 +0300

    Add files via upload

---
+++ TextEditor/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR1_TextEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Multiline = true;
+        //    BackColor = Color.Peru;
+            textBox1.Height = 390;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            if (openFileDialog1.FileName == String.Empty) return;
+            // Чтение текстового файла
+            try
+            {
+                var Читатель = new System.IO.StreamReader(
+                openFileDialog1.FileName, Encoding.GetEncoding(1251));
+                textBox1.Text = Читатель.ReadToEnd();
+                Читатель.Close();
+            }
+            catch (System.IO.FileNotFoundException Ситуация)
+            {
+                MessageBox.Show(Ситуация.Message + "\nНет такого файла",
+                         "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            catch (Exception Ситуация)
+            { // отчет о других ошибках
+                MessageBox.Show(Ситуация.Message,
+                     "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void сохранитьКакToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = openFileDialog1.FileName;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var Писатель = new System.IO.StreamWriter(
+                    saveFileDialog1.FileName, false,
+                                        System.Text.Encoding.GetEncoding(1251));
+                    Писатель.Write(textBox1.Text);
+                    Писатель.Close();
+                }
+                catch (Exception Ситуация)
+                { // отчет о других ошибках
+                    MessageBox.Show(Ситуация.Message,
+                        "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
+
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();   
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            openFileDialog1.FileName = @"data\Text2.txt";
+            openFileDialog1.Filter =
+                     "Текстовые файлы (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.Filter =
+                     "Текстовые файлы (*.txt)|*.txt|All files (*.*)|*.*";
+
+        }
+    }
+}
