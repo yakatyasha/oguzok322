77bdd8886c321e29ff56090e5eded5e042bd9311
yakatyasha <127683941+yakatyasha@users.noreply.github.com>
2023-05-20 08:48:43 +0300

    Add files via upload

---
+++ MakerScreens/WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public static Bitmap BM = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics GH = Graphics.FromImage(BM as Image);
+            GH.CopyFromScreen(0,0,0,0, BM.Size);
+
+            ScreenShot SI = new ScreenShot();
+
+            SI.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
